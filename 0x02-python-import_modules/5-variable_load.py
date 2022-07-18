b9a028e5c76fb55d0376ca4b822048ba1cf407a0
chinonsonweke <chinonsonweke01@gmail.com>
2022-07-18 05:12:08 -0700

    5. Everything can be imported

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python3
-import hidden_4
-
-
-def principal():
-    for i in dir(hidden_4):
-        if not (i[0] == '_' and i[1] == '_'):
-            print(i)
-
-
 if __name__ == "__main__":
-    principal()
+    from variable_load_5 import a
+    print("{:d}".format(a)
